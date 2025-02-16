095b170a1446ddf5303f38deb4b4725e09e20621
Neb <NebulaSkies00@gmail.com>
2025-02-16 11:45:31 -0600

    Text will be inverted based off the background

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const root = document.querySelector(':root');
 
 let time, sunrise, sunset, temperature, cloud, precipitation, wind
 
-time = Date.now()/1000
+time = Date.now() / 1000
 
 //time = new Date(2025, 1, 16, 1) //Test times
 //time = time.getTime()/1000
@@ -13,7 +13,7 @@ time = Date.now()/1000
 getWeather(35.19879047087778, -97.44495060227347)
 
 //Requests the API and fills the variables
-async function getWeather(latitude, longitude){
+async function getWeather(latitude, longitude) {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=apparent_temperature,precipitation,cloud_cover,wind_speed_10m&daily=sunrise,sunset&timeformat=unixtime&past_days=1&forecast_days=1`
 
     try {
@@ -44,14 +44,19 @@ async function getWeather(latitude, longitude){
 function updateStyle() {
     if (!sunrise) return //If there is nothing in sunrise, then the API was probably not reached
 
-    let background = new Color("skyblue")
+    let background = new Color('skyblue')
 
     background.hsv.v *= nightMultiplier()
 
-    background.hsv.v *= 1 - precipitation/150
-    background.hsv.s *= 1 - cloud/100
+    background.hsv.v *= 1 - precipitation / 150
+    background.hsv.s *= 1 - cloud / 100
 
     root.style.setProperty('--background', background.toString())
+    
+    //Invert the text's value color based off the background
+    let text = new Color('black')
+    text.hsv.v = 100-background.hsv.v
+    root.style.setProperty('--text', text.toString())
 }
 
 function updateInformation() {
@@ -64,6 +69,6 @@ function updateInformation() {
 //Returns a value to multiply by the background's light value
 function nightMultiplier() {
     return Math.min(Math.abs(
-        ((time - (sunrise+sunset)/2) / (sunrise - sunset))**0.5
+        ((time - (sunrise + sunset) / 2) / (sunrise - sunset)) ** 0.5
     ), 1)
 }
